06eaed1e52c975a599951c871e036770f83daa13
BagusFadhilHusain <115984543+BagusFadhilHusain@users.noreply.github.com>
2024-05-20 23:57:44 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.tugasday11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton homeButton, descriptionButton, widgetsButton, personButton , isiberandaButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Menginisialisasi tombol
+        homeButton = findViewById(R.id.homeButton);
+        descriptionButton = findViewById(R.id.descriptionButton);
+        widgetsButton = findViewById(R.id.widgetsButton);
+        personButton = findViewById(R.id.personButton);
+        isiberandaButton = findViewById(R.id.isiberandaButton);
+
+        // Mengatur OnClickListener untuk setiap tombol
+        klik();
+    }
+
+    void klik(){
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        descriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        widgetsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        personButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SayaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        isiberandaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IsiBerandaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ SayaActivity.java
@@ -0,0 +1,64 @@
+package com.example.tugasday11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SayaActivity extends AppCompatActivity {
+
+    ImageButton homeButton, descriptionButton, widgetsButton, personButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Menginisialisasi tombol
+        homeButton = findViewById(R.id.homeButton);
+        descriptionButton = findViewById(R.id.descriptionButton);
+        widgetsButton = findViewById(R.id.widgetsButton);
+        personButton = findViewById(R.id.personButton);
+
+        // Mengatur OnClickListener untuk setiap tombol
+        klik();
+    }
+
+    void klik(){
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SayaActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        descriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SayaActivity.this, FileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        widgetsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SayaActivity.this, AlatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        personButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SayaActivity.this, SayaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
